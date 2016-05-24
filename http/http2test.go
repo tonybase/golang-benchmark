7c6a62b95cffbde3048bdac46729a9948b272d95
Tony <i@tony.wiki>
2016-05-24 11:08:00 +0800

    add http test.

--- http/http2test.go
+++ http/http2test.go
@@ -6,26 +6,44 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
 
-const MAX = 10000
+const MAX = 3000
+
+var (
+	now          = time.Now()
+	count  int32 = 0
+	q            = make(chan int, 1)
+	client *http.Client
+)
 
 func main() {
 	tr := &http2.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
-	client := &http.Client{Transport: tr}
-	now := time.Now()
+	client = &http.Client{Transport: tr}
 	for i := 0; i < MAX; i++ {
-		resp, err := client.Get("https://s1.tony.wiki:8002/")
-		if err != nil {
-			log.Fatal(err)
-		}
-		_, err = ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
+		go reqHttp2()
 	}
-	log.Println("Max: ", MAX, " Time:", time.Now().Sub(now))
+	<-q
+}
+
+func reqHttp2() {
+	resp, err := client.Get("https://127.0.0.1:8002/")
+	if err != nil {
+		panic(err)
+	}
+	_, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	resp.Body.Close()
+
+	atomic.AddInt32(&count, 1)
+	if count == MAX {
+		log.Println("MAX: ", MAX, " Time:", time.Now().Sub(now))
+		q <- 1
+	}
 }
--- http/httptest.go
+++ http/httptest.go
@@ -4,22 +4,38 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
 
-const MAX = 10000
+const MAX = 3000
+
+var (
+	now         = time.Now()
+	count int32 = 0
+	q           = make(chan int, 1)
+)
 
 func main() {
-	now := time.Now()
-	for i := 0; i < MAX; i++ {
-		resp, err := http.Get("http://s1.tony.wiki:8001/")
-		if err != nil {
-			log.Fatal(err)
-		}
-		_, err = ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
+	for i := 0; i <= MAX; i++ {
+		go req()
 	}
-	log.Println("MAX: ", MAX, " Time:", time.Now().Sub(now))
+	<-q
+}
+
+func req() {
+	resp, err := http.Get("http://127.0.0.1:8001/")
+	if err != nil {
+		panic(err)
+	}
+	_, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	resp.Body.Close()
+	atomic.AddInt32(&count, 1)
+	if count == MAX {
+		log.Println("MAX: ", MAX, " Time:", time.Now().Sub(now))
+		q <- 1
+	}
 }
